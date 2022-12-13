c2309ab687245da11823133d264c0b6631dbf128
Mr. Scott <mscott@brightoncollege.net>
2022-12-13 16:31:07 +0000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='database_engine',
+    version='0.1',
+    packages=['database_engine'],
+    url='',
+    license='MIT Licence',
+    author='Matthew James Scott',
+    author_email='mscott@brightoncollege.net',
+    description='A tool to connect to a sqlite database'
+)
